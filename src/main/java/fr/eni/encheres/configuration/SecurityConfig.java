ddeda78c1cd87b1dea53e7a89bbdc882c5a3544b
MARIANA <marivitikhovich92@gmail.com>
2025-07-07 12:18:00 +0200

    Correction Spring Security

--- src/main/java/fr/eni/encheres/configuration/SecurityConfig.java
+++ src/main/java/fr/eni/encheres/configuration/SecurityConfig.java
@@ -1,13 +1,10 @@
 package fr.eni.encheres.configuration;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
