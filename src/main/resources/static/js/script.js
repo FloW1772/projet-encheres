1920a7aa752d286bd269907d59c1bc39f56f0a54
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2025-07-08 23:13:50 +0200

    mise a jour

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,21 @@
+function afficherFiltres() {
+    const radios = document.querySelectorAll('input[name="typeFiltre"]');
+    let typeFiltre = null;
+    radios.forEach(radio => {
+        if (radio.checked) {
+            typeFiltre = radio.value;
+        }
+    });
+    document.getElementById('filtresAchat').style.display = (typeFiltre === 'achat') ? 'block' : 'none';
+    document.getElementById('filtresVente').style.display = (typeFiltre === 'vente') ? 'block' : 'none';
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    afficherFiltres();
+
+
+    document.querySelectorAll('input[name="typeFiltre"]').forEach(radio => {
+        radio.addEventListener('change', afficherFiltres);
+    });
+});
